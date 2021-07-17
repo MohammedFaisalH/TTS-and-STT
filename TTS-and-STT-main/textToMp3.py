64ae7b012480afe7080c116063e173b45cbb4659
mohammed <78411621+MohammedFaisalH@users.noreply.github.com>
2021-07-17 18:44:58 +0300

    Add files via upload

---
+++ TTS-and-STT-main/textToMp3.py
@@ -0,0 +1,26 @@
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import TextToSpeechV1
+url = 'https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/0fc19f07-9bda-4069-b145-e643aad30bcb'
+apikey = 'uj1reAMFaMXaxEvHeftVld-Fss0w_GqrGZOiBMkSEcv8'
+
+
+def main():
+
+    # Setup Service
+    authenticator = IAMAuthenticator(apikey)
+    # New TTS Service
+    tts = TextToSpeechV1(authenticator=authenticator)
+    # Set Service URL
+    tts.set_service_url(url)
+
+    with open('text.txt', 'r') as f:
+        text = f.read()
+
+    with open('voice.mp3', 'wb') as audio_file:
+        res = tts.synthesize(text, accept='audio/mp3',
+                             voice='en-US_AllisonV3Voice').get_result()
+        audio_file.write(res.content)
+
+
+if __name__ == "__main__":
+    main()
